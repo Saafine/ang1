d46c9c3ea3d253b738f3b0c88907a9fd5b458950
lbs <pawel.lbs@gmail.com>
2017-07-09 12:50:26 +0200

    linting

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import './styles/main.less';
 import angular from 'angular';
 import ngRoute from 'angular-route';
 import ngAnimate from 'angular-animate';
-let myNinjaApp = angular.module('ang1', [ngRoute, ngAnimate]);
+let myAng1App = angular.module('ang1', [ngRoute, ngAnimate]);
 
-myNinjaApp.controller('controllerName', ['$scope', $scope => {
+myAng1App.controller('controllerName', ['$scope', $scope => {
 
 }]);
